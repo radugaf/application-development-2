f06c026c692a24f728602daab89dc7e607ae77c4
Radu Gafita <radugaf>
2020-12-29 04:37:08 +0200

    FINAL BACKEND

--- edesia_main/config/wsgi.py
+++ edesia_main/config/wsgi.py
@@ -1,18 +1,4 @@
-"""
-WSGI config for edesia_main project.
 
-This module contains the WSGI application used by Django's development server
-and any production WSGI deployments. It should expose a module-level variable
-named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
-this application via the ``WSGI_APPLICATION`` setting.
-
-Usually you will have the standard Django WSGI application here, but it also
-might make sense to replace the whole Django WSGI application with a custom one
-that later delegates to the Django one. For example, you could introduce WSGI
-middleware here, or combine a Django application with an application of another
-framework.
-
-"""
 import os
 import sys
 from pathlib import Path
@@ -28,7 +14,7 @@ sys.path.append(str(ROOT_DIR / "edesia_main"))
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "config.settings.production"
+    "DJANGO_SETTINGS_MODULE", "config.settings.local"
 )
 
 # This application object is used by any WSGI server configured to use this
